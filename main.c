cad21a1f23f87735a12803f804532d013483496a
mort <theojohnhenry@gmail.com>
2026-07-04 10:52:00 +0200

    ss

--- main.c
+++ main.c
@@ -1,25 +1,44 @@
 #include <raylib.h>
+#include <stdbool.h>
+#include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
 
 const int screen_width = 800;
 const int screen_height = 800;
 const float update_interval = 1.0f;
-static int I,J;
 
-void draw_data(int data[], int n, int spesh);
-void bubble_sort(int data[], int n);
-void test();
+typedef struct {
+    bool finished;
+    int i;
+    int pass;
+    int n;
+    int *data;
+} bubble_sort_state;
 
 
-int main()
+void draw_data(int data[], int n, bubble_sort_state *state);
+void seq_bubble_sort(int data[], int n, bubble_sort_state *state);
+void swap(int* xp, int* yp);
+
+int main(int argc, char *argv[])
     {
 
+    if(argc<1){printf("bad input");return 1;}
+    int n = argc-1;
+
+    int *data = malloc(n * sizeof(int));
+    if(data==NULL){printf("malloc error");return 1;}
+
+    for(int i = 0; i < n; i++)
+    {
+        printf("placing value %i into data slot %i \n", atoi(argv[i+1]), i);
+        data[i] = atoi(argv[i + 1]);
+    }
+
+    bubble_sort_state bss = {false, 0, 0, n, data};
+
     float time_counter = 0.0f;
-
-    int data[] = {12, 5, 6, 1 ,5 ,8 ,2 ,3 ,4, 9};
-    int n = sizeof(data)/sizeof(data[0]);
-
     InitWindow(screen_width, screen_height, "PENIS WINDOW");
     SetTargetFPS(60);
 
@@ -28,9 +47,9 @@ int main()
         time_counter += GetFrameTime();
         if(time_counter >= update_interval){
             time_counter = 0;
-            bubble_sort(data, n);
+            seq_bubble_sort(data, n, &bss);
         }
-        draw_data(data, n, I);
+        draw_data(data, n, &bss);
     }    
 
     CloseWindow(); 
@@ -38,20 +57,21 @@ int main()
     return 0;
 }
 
-void draw_data(int data[], int n, int spesh)
+void draw_data(int data[], int n, bubble_sort_state *state)
 {
 
+    int current = state->i;
+    int passed = n - state->pass;
     int staple_pixelwidth = screen_width/n;
 
-    ClearBackground(BLACK);
     BeginDrawing();
+    ClearBackground(BLACK);
     int i;
     for(i=0; i <= n-1; i++)
     {
         Color current_color = RAYWHITE;
-        if(i == spesh) {current_color = RED;}
-
-        //printf("DEBUG(drawing): i=%i, data=%i \n ", i, data[i]);
+        if(i == current) {current_color = RED;}
+        if(i >= passed || state->finished) {current_color = GREEN;}
 
         int current_posX = i*staple_pixelwidth;
         int current_height = data[i] * 40;
@@ -64,29 +84,45 @@ void draw_data(int data[], int n, int spesh)
                 current_color);
     }
 
-    DrawText(TextFormat("VAL = %i", data[I]), screen_width/2 - 22, screen_height/2 - 22, 22, RAYWHITE);
-    DrawText(TextFormat("I   = %i", I), screen_width/2, screen_height/2, 22, RAYWHITE);
+    DrawText(TextFormat("VAL = %i", data[current]), screen_width/2, screen_height/2 - 22, 22, RAYWHITE);
+    DrawText(TextFormat("I   = %i", current), screen_width/2, screen_height/2, 22, RAYWHITE);
     EndDrawing();
 }
 
-void bubble_sort(int data[], int n)
+void seq_bubble_sort(int *data, int n, bubble_sort_state *state)
 {
-    int temp;
-    int k = n;
 
-    if(k>=0)
+    if(state->finished)
     {
-        I = 0;
-        if(I < n-1)
-            {
-                if(data[I]>data[I+1])
-                {
-                    temp = data[I];
-                    data[I] = data[I+1];
-                    data[I+1] = temp;
-                }
-                I++;
-            }
+        return;
     }
-    k--;
+
+    int i = state->i;
+
+    if(data[i]>data[i+1])
+    {
+        swap(&data[i], &data[i+1]);
+    }
+
+    state->i++;
+
+    int comparisons_this_pass = n - 1 - state->pass;
+
+    if(state->i >= comparisons_this_pass)
+    {
+        state->i = 0;
+        state->pass++;
+    }
+    
+    if(state->pass >= n-1)
+    {
+        state->finished = true;
+    }
 }
+
+void swap(int* xp, int* yp){
+    int temp = *xp;
+    *xp = *yp;
+    *yp = temp;
+} 
+
